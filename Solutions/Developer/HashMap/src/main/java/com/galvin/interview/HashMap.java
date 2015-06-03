505a117a0607d0d7cc9e6335b73169428a57eee1
Thomas Galvin <tgalvin@caci.com>
2015-06-03 15:04:31 -0400

    Added documentation to HashMap class

--- Solutions/Developer/HashMap/src/main/java/com/galvin/interview/HashMap.java
+++ Solutions/Developer/HashMap/src/main/java/com/galvin/interview/HashMap.java
@@ -1,5 +1,37 @@
 package com.galvin.interview;
 
+/**
+ * This class implements an array-based Hash Table.
+ * 
+ * This implementation uses the following algorithm to store a key/value pair
+ *     1. Generate the hashcode of the key
+ *     2. If an Entry currently exists for that hashcode, replace it with a 
+ *        new Entry
+ *     3. If there is no Entry for that hashcode, create a new one and add
+ *        it to the array.
+ * 
+ * The array does not need to be sorted. If the capacity of the array is
+ * exceeded, it should be grown by a factor of MULTIPLIER, similar to an
+ * Array List implementation.
+ * 
+ * The get algorithm:
+ *     1. Calculate the hashcode of the key
+ *     2. Loop over the values in the entries array, comparing the Entry's
+ *        hashcode to the key's hashcode
+ *     3. If the hashcodes match, return the Entry's value
+ *     4. If no match is found, return null
+ * 
+ * The remove algorithm:
+ *     1. Calculate the hashcode of the key
+ *     2. Loop over the values in the entries array, comparing the Entry's
+ *        hashcode to the key's hashcode
+ *     3. If the hashcodes match:
+ *         A. Loop over the remainder of the array, shifting each value
+ *            one place to the left
+ *         B. Set the last element in the array to null
+ * 
+ * Your task is to implement the put(), get(), and remove() methods.
+ */
 public class HashMap {
     private static final int DEFAULT_SIZE = 5;
     private static final int MULTIPLIER = 2;
@@ -76,7 +108,6 @@ public class HashMap {
         }
     }
 
-    //TODO: sort and use binary search
     private int find( int keyHash ) {
         for( int i = 0; i <= index; i++ ) {
             if( entries[i] != null ) {
@@ -88,6 +119,11 @@ public class HashMap {
         return -1;
     }
 
+    /**
+     * An Entry holds a key/value pair; this object is then added to our 
+     * underlying array, allowing us to store the value and its key in an
+     * easily searchable format.
+     */
     private class Entry {
         private int keyHash;
         private Object value;
